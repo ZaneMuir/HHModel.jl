f3fc0ef849cb44721befcbbe587dd842bceb47f7
Yizhan Miao <yizhanmiao@pm.me>
2019-10-15 21:41:34 -0700

    update name

--- src/ChannelZoo.jl
+++ src/ChannelZoo.jl
@@ -12,7 +12,6 @@ $$i_{\text{ltk}} = g_{\text{ltk}} w^4 z (V - E_{\text{K}})$$
 <!-- TODO: ### complex version -->
 
 - modified from `inf_tau_w_ltk_rm.m`, `inf_tau_z_ltk_rm.m` and `I_ltk_rm.m` 
-
 """
 function low_voltage_gated_potassium(g::Real; iltkcomplex=false, subtype=:kv1)
     _param = (
@@ -24,18 +23,19 @@ function low_voltage_gated_potassium(g::Real; iltkcomplex=false, subtype=:kv1)
     
     if iltkcomplex
         #TODO
-        return SimpleIonChannel("low-voltage-gated potassium, complex", :potassium, 
+        return SimpleIonChannel("ltk_cmplx", :potassium, 
             g, 0, 0,
             Kinetics(), Kinetics())
     else
         # Change back to 100 for kv1 like
         _w_tau = (V) -> 100*(6*exp((V+60)/6)+16*exp(-(V+60)/45))^(-1)+1.5 # IKL 
-        _w = Kinetics(4, -44.5, 8.4, _tau = _w_tau)
+        _w_infty = (V) -> (1+exp(-(V+44.5)/8.4))^(-1/4)
+        _w = Kinetics(4, _w_infty, _w_tau)
         
         _z_tau = (V) -> _camp*(exp((V+60)/20)+exp(-(V+60)/8))^(-1)+50;
         _z = Kinetics(1, -71.0, 10.0, zeta=_zeta, _tau=_z_tau, state=:inactivation)
 
-        return SimpleIonChannel("low-voltage-gated potassium, simple", :potassium, 
+        return SimpleIonChannel("ltk", :potassium, 
             g, _w, _z)
     end
 end
@@ -44,7 +44,7 @@ end
 ## high voltage gated potassium channel
 
 """
-function high_voltage_gated_potassium(g::Real; phi=0.5)
+function high_voltage_gated_potassium(g::Real; phi=0.85)
     _n_tau = (V) -> 100*(11*exp((V+60)/24)+21*exp(-(V+60)/23))^(-1)+0.7;
     _n_infty = (V) -> (1+exp(-(V+15)/5))^(-1/2)
     _n = HHModel.Kinetics(2, _n_infty, _n_tau)
@@ -52,8 +52,8 @@ function high_voltage_gated_potassium(g::Real; phi=0.5)
     _p_tau = (V) -> 100*(4*exp((V+60)/32)+5*exp(-(V+60)/22))^(-1)+5;
     _p = HHModel.Kinetics(1, -23, 6, _tau=_p_tau)
 
-    HHModel.ComplexIonChannel("high voltage gated potassium", :potassium,
-           g, [phi, 1-phi],
+    HHModel.ComplexIonChannel("htk", :potassium,
+           g, [0.85, 0.15],
            [(_n, HHModel.Kinetics()), 
             (_p, HHModel.Kinetics())]
     )
@@ -73,7 +73,7 @@ function hh_sodium(g::Real)
     _h_tau = (V) -> 100 / (7*exp((V+60)/11)+10*exp(-(V+60)/25))+0.6
     _h = Kinetics(1, -65.0, 6.0, _tau=_h_tau, state=:inactivation)
     
-    SimpleIonChannel("hh sodium", :sodium, 
+    SimpleIonChannel("ina", :sodium, 
         g, _m, _h)
 end
 
@@ -100,7 +100,7 @@ function hh_potassium(g::Real)
     _c_infty = (V) -> (1+exp((V+66)/7))^(-1/2);
     _c = HHModel.Kinetics(1, _c_infty, _c_tau)
 
-    HHModel.GenericIonChannel("hh potassium", :potassium,
+    HHModel.GenericIonChannel("ik", :potassium,
         g, ik_rule,
         [_a, _b, _c]
     );
@@ -113,15 +113,16 @@ $$i_{\text{h}} = g_{\text{h}} r (V - E_{\text{h}})$$
 - modified from `inf_tau_r_rm.m` and `I_h_rm.m`
 """
 function ihcurrent(g::Real)
-    a=83;
-    b = 7.6;
-    c = 0.0002;
-    d = 5.3;
-    e=313;
-    _r_tau = (V) -> (1/c) * 1/(exp(-(V+a)/b) + exp((V+a)/d))+e;
-    _r = Kinetics(1, -100.0, 7.0, _tau=_r_tau, state=:inactivation)
+    a = 86.86183;
+     b = 5.76769;
+     c = 3393.34171;
+     d = 6.67393;
+     e = 295.95798;
+    # _r_tau = (V) -> (1/c) * 1/(exp(-(V+a)/b) + exp((V+a)/d))+e;
+    _r_tau = (V) -> ((1/c)*1/(exp(-(V+a)/b)) + (exp((V+a)/d)))+e;
+    _r = Kinetics(1, -96.11, 8.1, _tau=_r_tau, state=:inactivation)
     
-    SimpleIonChannel("ih current", :ih, 
+    SimpleIonChannel("ih", :ih, 
         g, Kinetics(), _r)
 end
 
